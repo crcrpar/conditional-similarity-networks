3ada8618e976fefd598469605b9da8116f7b4c63
crcrpar <kozukimasaki@KozukiMasaki-no-iMac.local>
2017-09-10 14:16:02 +0900

    add comments

--- zappos_data.py
+++ zappos_data.py
@@ -11,6 +11,7 @@ def default_image_loader(path):
 
 
 def check_paths(root, paths):
+    """Check whether the passed paths exist or not."""
     existing_files, non_existing_files = list(), list()
     for path in paths:
         if os.path.exists(os.path.join(root, path)):
@@ -22,6 +23,8 @@ def check_paths(root, paths):
 
 class ZapposDataset(torch.utils.data.Dataset):
 
+    """ZapposDataset return image and condition."""
+
     def __init__(self, root, base_path, files_json_path, condition,
                  conditions=None, transform=None, loader=default_image_loader):
         self.root = root
@@ -31,7 +34,8 @@ class ZapposDataset(torch.utils.data.Dataset):
             paths = [line.rstrip('\n') for line in f]
 
         self.files, self.non_existing_files = check_paths(self.img_root, paths)
-        print('\t+{} non existing files\n'.format(len(self.non_existing_files)))
+        if len(self.non_existing_files) > 0:
+            print('\t+{} non existing files\n'.format(len(self.non_existing_files)))
         self.condition = condition
         self.transform = transform
         self.loader = loader
@@ -53,6 +57,14 @@ class ZapposDataset(torch.utils.data.Dataset):
 
 
 def make_dataset(condition, root, base_path, files_json_path):
+    """Make a zappos dataset class
+
+    Args:
+        condition: for Zappos, should be in [0, 3].
+        root: root directory having zappos directory in it.
+        base_path: directory name of zappos dataset.
+        files_json_path: list of relative paths to images from base_path.
+    """
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     dataset = ZapposDataset(root, base_path, files_json_path, condition,
